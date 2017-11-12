903432876fd8e5c03c9115758078bf9e42fab59c
DmitryYaroslavtsev <yaroslav-demon@yandex.ru>
2017-11-12 21:58:56 +0300

    Demo of the class

---
+++ src/com/company/TwoVehicles.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class TwoVehicles {
+    public static void main(String args[]) {
+        Vehicle minivan = new Vehicle();
+        Vehicle sportcar = new Vehicle();
+
+        int range1, range2;
+
+        minivan.passengers = 7;
+        minivan.fuelcap = 16;
+        minivan.mpg = 21;
+
+        sportcar.passengers = 2;
+        sportcar.fuelcap = 14;
+        sportcar.mpg = 12;
+
+        range1 = minivan.fuelcap * minivan.mpg;
+        range2 = sportcar.fuelcap * sportcar.mpg;
+
+        System.out.println("Минивен может перевезти " + minivan.passengers + " пассажиров на расстояние " + range1 + " миль");
+        System.out.println("Спорткар может перевезти " + sportcar.passengers + " пассажиров на расстояние " + range2 + " миль");
+
+    }
+}
