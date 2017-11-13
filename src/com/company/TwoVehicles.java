76036120f6106caf49ce0d3dad5404912b4364c6
DmitryYaroslavtsev <yaroslav-demon@yandex.ru>
2017-11-13 22:45:57 +0300

    Demo of the class

--- src/com/company/TwoVehicles.java
+++ src/com/company/TwoVehicles.java
@@ -2,19 +2,11 @@ package com.company;
 
 public class TwoVehicles {
     public static void main(String args[]) {
-        Vehicle minivan = new Vehicle();
-        Vehicle sportcar = new Vehicle();
+        Vehicle minivan = new Vehicle(7, 16, 21);
+        Vehicle sportcar = new Vehicle(2, 14, 12);
 
         int range1, range2;
         int dist = 252;
-
-        minivan.passengers = 7;
-        minivan.fuelcap = 16;
-        minivan.mpg = 21;
-
-        sportcar.passengers = 2;
-        sportcar.fuelcap = 14;
-        sportcar.mpg = 12;
 
         range1 = minivan.range();
         range2 = sportcar.range();
--- src/com/company/VehicleDemo.java
+++ src/com/company/VehicleDemo.java
@@ -2,12 +2,8 @@ package com.company;
 
 public class VehicleDemo {
     public static void main(String args[]) {
-        Vehicle minivan = new Vehicle();
+        Vehicle minivan = new Vehicle(7, 16, 21);
         int range;
-
-        minivan.passengers = 7;
-        minivan.fuelcap = 16;
-        minivan.mpg = 21;
 
         range = minivan.fuelcap * minivan.mpg;
         System.out.println("Минивен может перевезти " + minivan.passengers + " пассажиров на расстояние " + range + " миль");
