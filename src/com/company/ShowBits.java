fece94f2f47ece8be996803e75a138fc38a841a0
DmitryYaroslavtsev <yaroslav-demon@yandex.ru>
2017-11-19 10:05:18 +0300

    ForEach and Strings

---
+++ src/com/company/ShowBits.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class ShowBits {
+    public static void main(String args[]) {
+        byte val = 123;
+
+        for (int i = 128; i > 0; i/=2) {
+            if ((val & i) != 0) System.out.print("1");
+            else System.out.print("0");
+        }
+    }
+}
---
+++ src/com/company/upCase.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class upCase {
+    public static void main(String args[]) {
+        char ch;
+
+        for (int i = 0; i < 10; i++) {
+            ch = (char) ('a' + i);
+            System.out.print(ch);
+
+            ch = (char) ((int)ch & 65503);
+
+            System.out.print(ch + " ");
+        }
+    }
+}
