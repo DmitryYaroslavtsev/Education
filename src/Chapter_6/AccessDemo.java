8a68c9507ed348fe404fdba238eeb13ac453fc65
DmitryYaroslavtsev <yaroslav-demon@yandex.ru>
2017-11-21 06:09:50 +0300

    Chapter 6

--- src/Chapter_6/AccessDemo.java
+++ src/Chapter_6/AccessDemo.java
@@ -1,4 +1,17 @@
 package Chapter_6;
 
+class MyClass {
+    private int alpha;
+    public int beta;
+    int gamma;
+
+    void setAlpha(int a) {
+        alpha = a;
+    }
+    int getAlpha() {
+        return alpha;
+    }
+}
+
 public class AccessDemo {
 }
