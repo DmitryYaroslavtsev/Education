6b30546027036975a8a33536f5342ee2d45a4559
DmitryYaroslavtsev <yaroslav-demon@yandex.ru>
2017-11-21 23:34:41 +0300

    Chapter 6

---
+++ src/Chapter_6/CallByRef.java
@@ -0,0 +1,25 @@
+package Chapter_6;
+
+class Test1 {
+    int a, b;
+
+    Test1(int i, int j) {
+        a = i;
+        b = j;
+    }
+
+    void change(Test1 ob) {
+        ob.a = ob.a + ob.b;
+        ob.b = -ob.b;
+    }
+}
+
+public class CallByRef {
+    public static void main(String args[]) {
+        Test1 ob = new Test1(15, 20);
+
+        System.out.println("ob.a и ob.b до вызова: " + ob.a + " " + ob.b);
+        ob.change(ob);
+        System.out.println("a и b после вызова: " + ob.a + " " + ob.b);
+    }
+}
