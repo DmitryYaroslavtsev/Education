71b08af555d47e1fa734d5821d6866d6a3fa0a3a
DmitryYaroslavtsev <yaroslav-demon@yandex.ru>
2017-11-27 19:00:19 +0300

    test

--- src/chapter_8/UseBook.java
+++ src/chapter_8/UseBook.java
@@ -1,16 +1,17 @@
 package chapter_8;
 
+import bookpack.*;
+
 public class UseBook {
     public static void main(String args[]) {
         bookpack.Book books[] = new bookpack.Book[5];
-        books[0] = new bookpack.Book("1","1", 1);
-        books[1] = new bookpack.Book("2", "2", 2);
-        books[2] = new bookpack.Book("3", "3",3);
-        books[3] = new bookpack.Book("4", "4", 4);
-        books[4] = new bookpack.Book("5", "5", 5);
+        books[0] = new Book("1","1", 1);
+        books[1] = new Book("2", "2", 2);
+        books[2] = new Book("3", "3",3);
+        books[3] = new Book("4", "4", 4);
+        books[4] = new Book("5", "5", 5);
 
-        for (int i = 0; i < books.length; i++) {
-            books[i].show();
-        }
+
+        for (Book b: books) {b.show();}
     }
 }
