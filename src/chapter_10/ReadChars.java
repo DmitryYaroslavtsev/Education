ee55e330f412a5a15e352f4c81bce777650ef83a
DmitryYaroslavtsev <yaroslav-demon@yandex.ru>
2017-12-02 21:02:48 +0300

    Chapter 10

--- src/chapter_10/ReadChars.java
+++ src/chapter_10/ReadChars.java
@@ -1,8 +1,6 @@
 package chapter_10;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class ReadChars {
     public static void main(String[] args) throws IOException {
