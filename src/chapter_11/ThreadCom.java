fcec35d7954585d211d70a7f6f6f6006d018d462
DmitryYaroslavtsev <yaroslav-demon@yandex.ru>
2017-12-05 22:53:59 +0300

    Chapter 11

---
+++ src/chapter_11/ThreadCom.java
@@ -0,0 +1,79 @@
+package chapter_11;
+
+class TickTock {
+    String state;
+
+    synchronized void tick(boolean running) {
+        if (!running) {
+            state = "ticked";
+            notify();
+            return;
+        }
+        System.out.print("Tick ");
+
+        state = "ticked";
+        notify();
+        try {
+            while (!state.equals("tocked"))
+                wait();
+        } catch (InterruptedException exc) {
+            System.out.println("Прерывание потока");
+        }
+    }
+
+    synchronized void tock(boolean running) {
+        if (!running) {
+            state = "tocked";
+            notify();
+            return;
+        }
+
+        System.out.println("Tock");
+        state = "tocked";
+        notify();
+
+        try {
+            while (!state.equals("ticked"))
+                wait();
+        } catch (InterruptedException exc) {
+            System.out.println("Прерывание потока");
+        }
+    }
+}
+
+class MyThread4 implements Runnable {
+    Thread thrd;
+    TickTock ttOb;
+
+    MyThread4(String name, TickTock tt) {
+        thrd = new Thread(this, name);
+        ttOb = tt;
+        thrd.start();
+    }
+
+    public void run() {
+        if (thrd.getName().compareTo("Tick") == 0) {
+            for (int i = 0; i < 5; i++) ttOb.tick(true);
+            ttOb.tick(false);
+        }
+        else {
+            for (int i = 0; i < 5; i++) ttOb.tock(true);
+            ttOb.tock(false);
+        }
+    }
+}
+
+public class ThreadCom {
+    public static void main(String[] args) {
+        TickTock tt = new TickTock();
+        MyThread4 mt1 = new MyThread4("Tick",tt);
+        MyThread4 mt2 = new MyThread4("Tock", tt);
+
+        try {
+            mt1.thrd.join();
+            mt2.thrd.join();
+        } catch (InterruptedException exc) {
+            System.out.println("Прерывание основного потока");
+        }
+    }
+}
