b0bcf18b888af6fdea065c7e1c9231cfab82176d
DmitryYaroslavtsev <yaroslav-demon@yandex.ru>
2017-12-07 23:41:52 +0300

    Chapter 13

---
+++ src/chapter_13/GenDemo.java
@@ -0,0 +1,40 @@
+package chapter_13;
+
+class Gen<T> {
+    T ob;
+
+    Gen(T o) {
+        ob = o;
+    }
+
+    T getOb() {
+        return ob;
+    }
+
+    void showType() {
+        System.out.println("Тип Т - это " +
+                ob.getClass().getName());
+    }
+}
+
+public class GenDemo {
+    public static void main(String[] args) {
+
+        Gen<Integer> iOb;
+
+        iOb = new Gen<Integer>(88);
+
+        iOb.showType();
+
+        int v = iOb.getOb();
+        System.out.println("Value: " + v);
+        System.out.println();
+
+        Gen<String> strOb = new Gen<String>("Test");
+
+        strOb.showType();
+
+        String str = strOb.getOb();
+        System.out.println("Value: " + str);
+    }
+}
