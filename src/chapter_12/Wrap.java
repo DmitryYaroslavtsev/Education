79861859e7f07eafecd82c8722aab77ed50688bb
DmitryYaroslavtsev <yaroslav-demon@yandex.ru>
2017-12-07 20:16:59 +0300

    Chapter 12

---
+++ src/chapter_12/Wrap.java
@@ -0,0 +1,12 @@
+package chapter_12;
+
+public class Wrap {
+    public static void main(String[] args) {
+
+        Integer iOb = new Integer(100);
+
+        int i = iOb.intValue();
+
+        System.out.printf(i + " " +iOb);
+    }
+}
