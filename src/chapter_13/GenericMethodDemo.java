f798b5be42dd2b4e09159102e0f82e45ba3170e0
DmitryYaroslavtsev <yaroslav-demon@yandex.ru>
2017-12-10 23:57:25 +0300

    Chapter 13

--- src/chapter_13/GenericMethodDemo.java
+++ src/chapter_13/GenericMethodDemo.java
@@ -2,4 +2,14 @@ package chapter_13;
 
 public class GenericMethodDemo {
 
+    static <T extends Comparable<T>, V extends T> boolean
+    arraysEqual(T[] x, V[] y) {
+        if (x.length != y.length) return false;
+
+        for (int i = 0; i < x.length; i++) {
+            if (!x[i].equals(y[i]))
+                return false;
+
+        } return true;
+    }
 }
