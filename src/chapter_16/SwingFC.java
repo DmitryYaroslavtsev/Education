870abca138ac776d4405b5d80ae29cc71b0aab3e
DmitryYaroslavtsev <yaroslav-demon@yandex.ru>
2017-12-20 00:07:28 +0300

    Chapter 16

--- src/chapter_16/SwingFC.java
+++ src/chapter_16/SwingFC.java
@@ -47,6 +47,37 @@ public class SwingFC implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent ae) {
+        int i = 0, j = 0;
 
+        if (jtfFirst.getText().equals("")) {
+            jlabFirst.setText("First file name missing");
+            return;
+        }
+        if (jtfSecond.getText().equals("")) {
+            jlabSecond.setText("Second file name missing");
+            return;
+        }
+
+        try (FileInputStream f1 = new FileInputStream(jtfFirst.getText());
+             FileInputStream f2 = new FileInputStream(jtfSecond.getText())) {
+
+            do {
+                i = f1.read();
+                j = f2.read();
+                if (i != j) break;
+            } while (i != -1 && j != -1);
+
+            if (i != j)
+                jlabResult.setText("Files are not same");
+            else
+                jlabResult.setText("Files compare equal");
+
+        } catch (IOException exc) {
+            jlabResult.setText("File error");
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(SwingFC::new);
     }
 }
